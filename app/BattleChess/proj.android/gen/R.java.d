a2aae5d203e064638696e168a8bb3b40a5c8ffd2
Pol Monsó Purtí <pol.monso@gmail.com>
2014-05-27 18:15:00 +0200

    update gitignore, remove 'gen' dir

--- app/BattleChess/proj.android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/gen/org/sxbn/battlechess/R.java \
- : /Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/res/drawable-hdpi/icon.png \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/res/drawable-ldpi/icon.png \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/res/drawable-mdpi/icon.png \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/res/values/strings.xml \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/bin/res/drawable-hdpi/icon.png \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/bin/res/drawable-ldpi/icon.png \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/bin/res/drawable-mdpi/icon.png \
-/Users/monso/code/android/cocos2d-x-2.2.3/samples/Cpp/BattleChess/proj.android/bin/AndroidManifest.xml \
