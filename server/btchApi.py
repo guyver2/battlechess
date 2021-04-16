93cb563b1a20d3cc42eac41779c036d124408a89
Antoine Letouzey <antoine.letouzey@gmail.com>
2021-04-16 21:53:43 +0200

    minor changes + helper functions to convert to and from dict

--- server/btchApi.py
+++ server/btchApi.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta, timezone
-from typing import Optional, Tuple, Set
+from typing import Dict, Optional, Tuple, Set
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -14,14 +15,16 @@ fake_users_db = {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "hashed_password":
+        "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     },
     "janedoe": {
         "username": "janedoe",
         "full_name": "Jane Doe",
         "email": "janedoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "hashed_password":
+        "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     }
 }
@@ -31,10 +34,25 @@ fake_games_db = {
         "handle": "lkml4a3.d3",
         "white": "johndoe",
         "black": "janedoe",
-        "status": "idle", # idle, running, completed
-        "create_time": datetime(2021,1,1,tzinfo=timezone.utc),
+        "status": "started",
+        "create_time": datetime(2021, 1, 1, tzinfo=timezone.utc),
+    },
+    "da39a3ee5e": {
+        "handle": "da39a3ee5e",
+        "white": "johndoe",
+        "black": "janedoe",
+        "status": "running",
+        "create_time": datetime(2021, 3, 12, tzinfo=timezone.utc),
+    },
+    "d3255bfef9": {
+        "handle": "d3255bfef9",
+        "white": "johndoe",
+        "black": None,
+        "status": "open",
+        "create_time": datetime(2021, 4, 5, tzinfo=timezone.utc),
     }
 }
+
 
 class Token(BaseModel):
     access_token: str
@@ -44,21 +62,64 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     username: Optional[str] = None
 
+
 class User(BaseModel):
     username: str
     email: Optional[str] = None
     full_name: Optional[str] = None
     disabled: Optional[bool] = None
+    hashed_password: Optional[str] = None
+
+    def from_dict(self, data: dict) -> None:
+        try:
+            self.username = data["username"]
+            self.email = data["email"]
+            self.full_name = data["full_name"]
+            self.disabled = data["disabled"]
+            self.hashed_password = data["hashed_password"]
+        except:
+            pass
+
+    def to_dict(self) -> dict:
+        return {
+            "username": self.username,
+            "email": self.email,
+            "full_name": self.full_name,
+            "disabled": self.disabled,
+            "hashed_password": self.hashed_password,
+        }
+
 
 class UserInDB(User):
     hashed_password: str
 
+
 class Game(BaseModel):
-    handle: str
-    white: str
-    black: str
-    status: str
+    handle: str = None
+    white: str = None
+    black: str = None
+    status: str = None
     create_time: Optional[datetime] = None
+
+    def from_dict(self, data: dict) -> None:
+        try:
+            self.handle = data["handle"]
+            self.white = data["white"]
+            self.black = data["black"]
+            self.status = data["status"]
+            self.create_time = data["create_time"]
+        except:
+            pass
+
+    def to_dict(self) -> dict:
+        return {
+            "handle": self.handle,
+            "white": self.white,
+            "black": self.black,
+            "status": self.status,
+            "create_time": self.create_time,
+        }
+
 
 class Move(BaseModel):
     origin: Tuple[int, int]
@@ -71,6 +132,16 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+# allow for CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 def set_fake_db(fake_db):
     global fake_users_db
@@ -79,8 +150,10 @@ def set_fake_db(fake_db):
     print(fake_users_db)
     print(fake_db)
 
+
 def get_fake_db():
     return fake_users_db
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -95,6 +168,7 @@ def get_user(db, username: str):
         user_dict = db[username]
         return UserInDB(**user_dict)
 
+
 def add_user(db, user: UserInDB):
     fake_users_db[user.username] = {
         "username": user.username,
@@ -103,6 +177,7 @@ def add_user(db, user: UserInDB):
         "hashed_password": user.hashed_password,
         "disabled": False,
     }
+
 
 def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
@@ -123,9 +198,11 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 def create_game():
     # TODO generate links
     return "https://bt.ch/lkml4a3.d3"
+
 
 def get_game(gameUUID):
     game_exception = HTTPException(
@@ -135,6 +212,7 @@ def get_game(gameUUID):
     if gameUUID not in fake_games_db:
         raise game_exception
     return Game(**fake_games_db[gameUUID])
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -155,12 +233,16 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
+
+async def get_current_active_user(
+        current_user: User = Depends(get_current_user)):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-async def get_active_game(gameUUID, current_user: User = Depends(get_current_active_user)):
+
+async def get_active_game(
+    gameUUID, current_user: User = Depends(get_current_active_user)):
     game = get_game(gameUUID)
 
 
@@ -168,9 +250,12 @@ async def get_active_game(gameUUID, current_user: User = Depends(get_current_act
 async def version():
     return {'version': "1.0"}
 
+
 @app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username,
+                             form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -178,22 +263,30 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.username},
+                                       expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
+
 
 @app.get("/users/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
+
 @app.get("/users/me/games/")
-async def read_own_games(current_user: User = Depends(get_current_active_user)):
-    return [Game(**game) for gameName, game in fake_games_db.items() if game['white'] == current_user.username or game['black'] == current_user.username]
+async def read_own_games(
+        current_user: User = Depends(get_current_active_user)):
+    return [
+        Game(**game) for gameName, game in fake_games_db.items()
+        if game['white'] == current_user.username
+        or game['black'] == current_user.username
+    ]
+
 
 @app.get("/users/")
 async def get_users(current_user: User = Depends(get_current_active_user)):
     return list(fake_users_db.keys())
+
 
 @app.post("/users/")
 async def create_user(new_user: UserInDB):
@@ -209,13 +302,16 @@ async def create_user(new_user: UserInDB):
         add_user(fake_users_db, new_user)
         return get_user(fake_users_db, new_user.username)
 
+
 @app.get("/games/")
 async def get_new_game(current_user: User = Depends(get_current_active_user)):
     handle = create_game()
     return {"game_handle": handle}
 
+
 @app.get("/games/{gameUUID}")
-async def get_game_by_handle(gameUUID: str, current_user: User = Depends(get_current_active_user)):
+async def get_game_by_handle(
+    gameUUID: str, current_user: User = Depends(get_current_active_user)):
     game = get_active_game(gameUUID, current_user)
     if not game:
         raise HTTPException(
@@ -224,9 +320,12 @@ async def get_game_by_handle(gameUUID: str, current_user: User = Depends(get_cur
             headers={"WWW-Authenticate": "Bearer"},
         )
     return game
-    
+
+
 @app.post("/games/{gameUUID}/move")
-async def post_move(gameUUID: str, current_user: User = Depends(get_current_active_user), current_game: Game = Depends(get_active_game)):
+async def post_move(gameUUID: str,
+                    current_user: User = Depends(get_current_active_user),
+                    current_game: Game = Depends(get_active_game)):
     game = get_active_game(gameUUID, current_user)
     if not game:
         raise HTTPException(
@@ -235,4 +334,3 @@ async def post_move(gameUUID: str, current_user: User = Depends(get_current_acti
             headers={"WWW-Authenticate": "Bearer"},
         )
     return game
-    
