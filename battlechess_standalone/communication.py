6dfe6436a6dffe0db08cd7260178393901c9aaf1
quimnuss <quimnuss@petit>
2023-11-24 22:08:12 +0100

    move battlechess standalone (v1) to another directory

--- battlechess_standalone/communication.py
+++ battlechess_standalone/communication.py
@@ -1,4 +1,4 @@
-KNOWN_HEADERS = ['NICK', 'COLR', 'OVER', 'URLR', 'MOVE', 'BORD', 'VALD']
+KNOWN_HEADERS = ["NICK", "COLR", "OVER", "URLR", "MOVE", "BORD", "VALD"]
 
 # NICK : Nickname, data is a string
 # COLR : Player color, data is a string
@@ -11,43 +11,43 @@ KNOWN_HEADERS = ['NICK', 'COLR', 'OVER', 'URLR', 'MOVE', 'BORD', 'VALD']
 
 # generic tow way conversion from raw data to string representation
 def dataToString(header, data):
-    if header in ['NICK', 'COLR', 'URLR', 'BORD']:
+    if header in ["NICK", "COLR", "URLR", "BORD"]:
         return data
-    elif header == 'OVER':
-        return 'None'
-    elif header == 'MOVE':
-        return str(data[0])+str(data[1])+str(data[2])+str(data[3])
-    elif header == 'VALD':
+    elif header == "OVER":
+        return "None"
+    elif header == "MOVE":
+        return str(data[0]) + str(data[1]) + str(data[2]) + str(data[3])
+    elif header == "VALD":
         if data:
-            return 'T'
+            return "T"
         else:
-            return 'F'
+            return "F"
     else:
-        print('Unknown message type :', header)
+        print("Unknown message type :", header)
         raise ValueError()
 
 
 # and from string to raw data
 def stringToData(header, data):
-    if header in ['NICK', 'COLR', 'URLR', 'BORD']:
+    if header in ["NICK", "COLR", "URLR", "BORD"]:
         return data
-    elif header == 'OVER':
+    elif header == "OVER":
         return None
-    elif header == 'MOVE':
+    elif header == "MOVE":
         return [int(data[0]), int(data[1]), int(data[2]), int(data[3])]
-    elif header == 'VALD':
-        return data == 'T'
+    elif header == "VALD":
+        return data == "T"
     else:
-        print('Unknown message type :', header)
+        print("Unknown message type :", header)
         raise ValueError()
 
 
 # generic receive function
 def myreceive(sock, MSGLEN):
-    msg = ''
+    msg = ""
     while len(msg) < MSGLEN:
-        chunk = sock.recv(MSGLEN-len(msg))
-        if chunk == '':
+        chunk = sock.recv(MSGLEN - len(msg))
+        if chunk == "":
             raise RuntimeError("socket connection broken")
         msg = msg + chunk.decode("utf-8")
     return msg
@@ -60,19 +60,19 @@ def sendData(sock, header, data):
     pack += "%05d" % (len(datas))
     pack += datas
     # sock.send(pack)
-    sock.send(pack.encode('utf-8'))
+    sock.send(pack.encode("utf-8"))
     if header not in KNOWN_HEADERS:
-        print('Unknown message type :', header)
+        print("Unknown message type :", header)
 
 
 # receive a message and return the proper object
 def recvData(sock):
     header = myreceive(sock, 4)
-    size   = int(myreceive(sock, 5))
-    datas  = myreceive(sock, size)
-    data   = stringToData(header, datas)
+    size = int(myreceive(sock, 5))
+    datas = myreceive(sock, size)
+    data = stringToData(header, datas)
     if header not in KNOWN_HEADERS:
-        print('Unknown message type :', header)
+        print("Unknown message type :", header)
     return list([header, data])
 
 
